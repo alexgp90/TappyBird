793f8047aba4d46b060f9355715387e630338f43
alexgp90 <C0deofarms>
2016-09-27 13:53:21 +0300

    day 7 done

--- core/src/com/kilobolt/gameworld/GameWorld.java
+++ core/src/com/kilobolt/gameworld/GameWorld.java
@@ -3,6 +3,7 @@ package com.kilobolt.gameworld;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Rectangle;
 import com.kilobolt.gameobjects.Bird;
+import com.kilobolt.gameobjects.ScrollHandler;
 
 /**
  * Created by alexandru.pavel on 16.09.2016.
@@ -10,18 +11,23 @@ import com.kilobolt.gameobjects.Bird;
 public class GameWorld {
 
     private Bird bird;
-
+    private ScrollHandler scroller;
 
     public GameWorld(int midPointY) {
         bird = new Bird(33, midPointY - 5, 17, 12);
+        scroller = new ScrollHandler(midPointY +66);
     }
 
     public void update(float delta) {
         bird.update(delta);
+        scroller.update(delta);
     }
 
     public Bird getBird(){
         return bird;
+    }
+    public  ScrollHandler getScroller(){
+        return scroller;
     }
 
 }
